fde69dc804d48e0f0391e56fcf836dc481a4debf
mikouei <mikouei1@gmail.com>
2026-02-02 20:43:03 +0000

    Update api.js

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_BASE_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
+const API_BASE_URL =
+  process.env.EXPO_PUBLIC_BACKEND_URL ||
+  "https://association1.onrender.com";
+
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,

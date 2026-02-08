ea0cefa6eaf09d12b1e30c80a804fb243106c1e7
Rodrigue Angbo <mikouei1@gmail.com>
2026-02-08 18:14:44 +0000

    fix(api): remove double /api in baseURL

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import axios from "axios";
+
 const API_BASE_URL =
   process.env.EXPO_PUBLIC_BACKEND_URL ||
-  "https://association1.onrender.com/api";
+  "https://association1.onrender.com";
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
   timeout: 30000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 

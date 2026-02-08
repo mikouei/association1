ce45b3fe52b5cb02b6e4083dcb31f8c96ef7da7f
Rodrigue Angbo <mikouei1@gmail.com>
2026-02-08 18:21:55 +0000

    fix(web): remove duplicate axios import

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import axios from "axios";
-
 const API_BASE_URL =
   process.env.EXPO_PUBLIC_BACKEND_URL ||
   "https://association1.onrender.com";

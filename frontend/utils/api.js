553161f21c7e88495f83c3a9b9136e184d216e88
Rodrigue Angbo <mikouei1@gmail.com>
2026-02-08 20:34:42 +0000

    fix(api): remove double /api in baseURL

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const API_BASE_URL =
   process.env.EXPO_PUBLIC_BACKEND_URL ||
@@ -12,43 +12,5 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
-
-// Intercepteur pour ajouter le token automatiquement
-api.interceptors.request.use(
-  async (config) => {
-    // Toujours déterminer le bon token depuis AsyncStorage
-    // Ne JAMAIS faire confiance à api.defaults.headers.common['Authorization']
-    const authToken = await AsyncStorage.getItem('authToken');
-    const platformToken = await AsyncStorage.getItem('platformToken');
-    
-    // Déterminer quel token utiliser selon la route
-    const isPlatformRoute = config.url?.includes('/platform');
-    
-    if (isPlatformRoute && platformToken) {
-      config.headers.Authorization = `Bearer ${platformToken}`;
-    } else if (authToken) {
-      config.headers.Authorization = `Bearer ${authToken}`;
-    } else if (platformToken) {
-      config.headers.Authorization = `Bearer ${platformToken}`;
-    }
-    
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
-// Intercepteur pour gérer les erreurs globalement
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response?.status === 401) {
-      // Ne pas effacer les tokens automatiquement pour éviter les problèmes de conflit
-      console.log('401 error - token might be invalid');
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default api;
